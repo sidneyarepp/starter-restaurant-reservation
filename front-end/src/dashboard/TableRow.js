84b0dc737d22c0c9ecf4c40d7aa079fa9452614c
sidneyarepp <sidneyarepp@outlook.com>
2021-11-09 19:53:52 -0500

    Adding table status to the dashboard

--- front-end/src/dashboard/TableRow.js
+++ front-end/src/dashboard/TableRow.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 function TableRow({ table }) {
 
-    const { table_id, table_name, capacity } = table;
+    const { table_id, table_name, capacity, table_availability } = table;
 
     return (
         <tr>
             <td>{table_id}</td>
             <td>{table_name}</td>
             <td>{capacity}</td>
+            <td>{table_availability}</td>
         </tr>
     )
 }
